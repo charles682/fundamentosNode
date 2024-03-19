e2516059c44c254773e9efbd12c90094f5b4e0ff
Charles Melo <melocharles220@gmail.com>
2024-03-19 14:59:06 -0300

    HTTP Code

--- src/server.js
+++ src/server.js
@@ -1,5 +1,3 @@
-import http from 'node:http'
-
 // - Criar usuários
 // - Listagem usuários
 // - Edição de usuários
@@ -19,28 +17,33 @@ import http from 'node:http'
 // GET /users => Buscando usuários no banc-end
 // POST /users => Criar um usuário no back-end
 
-const users = []
+import http from 'node:http';
+ // Importa o módulo HTTP do Node.js para criar um servidor HTTP
 
-const server = http.createServer((req, res) => {
-    const {method, url} = req
+const users = []; // Declara um array vazio para armazenar usuários
 
-    if(method === 'GET' && url === '/users'){
-        return res
-        .setHeader('Content-type', 'application/json') 
-        .end(JSON.stringify(users))
+const server = http.createServer((req, res) => { // Cria um servidor HTTP
+    const {method, url} = req; // Extrai os campos 'method' e 'url' da requisição HTTP
+
+    // Verifica se a requisição é um método GET para a rota '/users'
+    if (method === 'GET' && url === '/users') {
+        res.setHeader('Content-type', 'application/json'); // Define o cabeçalho da resposta para JSON
+        return res.end(JSON.stringify(users)); // Retorna a lista de usuários em formato JSON
     }
 
-    if(method === 'POST' && url ==='/users'){
+    // Verifica se a requisição é um método POST para a rota '/users'
+    if (method === 'POST' && url === '/users') {
+        // Adiciona um novo usuário ao array 'users'
         users.push({
             id: 1,
-            name: 'john Doe',
-            email: 'johndoes@gmail.com'
-        })
+            name: 'John Doe',
+            email: 'johndoe@gmail.com'
+        });
 
-        return res.end('Criação de usuario')
+        return res.writeHead(201).end(); // Retorna um status 201 (Created) indicando sucesso
     }
 
-    return res.end("hello word")
-})
+    return res.writeHead(404).end('NOT FOUND'); // Retorna um status 404 (Not Found) se a rota não foi encontrada
+});
 
-server.listen(3333)
+server.listen(3333); // Faz o servidor escutar n
